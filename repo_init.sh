2bffcaba00248616aee4ba63de16dd932e925a7a
Adrien Bioteau <adrien.bioteau@gmail.com>
2017-11-29 16:37:32 +0100

    Add support of local repository mirror

--- repo_init.sh
+++ repo_init.sh
@@ -2,24 +2,29 @@
 
 NB_CORES=`cat /proc/cpuinfo | grep processor | wc -l`
 
-if [ $# -ne 1 ]
+if [ $# -ne 5 ]
 then
-    echo "[USAGE] ./repo_init.sh <aosp_workspace>"
+    echo "[USAGE] ./repo_init.sh <aosp_workspace> <aosp_mirror_url> <repo_mirror_url> <github_mirror_url> <git_branch>"
     exit 1
 fi
 
 AOSP_WORKSPACE=$1
+AOSP_MIRROR_URL=$2
+REPO_MIRROR_URL=$3
+GITHUB_MIRROR_URL=$4
+GIT_BRANCH=$5
+
 AOSP_TAG="android-8.0.0_r30"
 LOCAL_MANIFESTS_BRANCH="master"
 
 # Initialize the AOSP tree
 mkdir -p $AOSP_WORKSPACE
 cd $AOSP_WORKSPACE
-~/bin/repo init -u https://android.googlesource.com/platform/manifest.git -b $AOSP_TAG
+~/bin/repo init -u $AOSP_MIRROR_URL/platform/manifest.git --repo-url $REPO_MIRROR_URL/git-repo.git -b $AOSP_TAG
 
 # Add local_manifests
 cd .repo
-git clone https://github.com/abioteau/local_manifests
+git clone $GITHUB_MIRROR_URL/abioteau/local_manifests
 cd local_manifests
 git checkout $LOCAL_MANIFESTS_BRANCH
 cd ../..
@@ -28,5 +33,6 @@ cd ../..
 ~/bin/repo sync -j $NB_CORES
 
 # Apply AOSP patches
+~/bin/repo start $GIT_BRANCH --all
 ./repo_update.sh
-
+~/bin/repo prune
